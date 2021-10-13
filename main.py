48638e17309d38b0e557f11eb160541f1cd349d7
Anastasia <sohypmotizin@gmail.com>
2021-10-13 17:54:17 +0200

    Change postgresql url

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ Bootstrap(app)
 
 # CONNECT TO DB
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
